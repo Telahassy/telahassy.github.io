dbdbc553d8898ae1a1f0399aed2735d839252785
Telahassy <58189820+Telahassy@users.noreply.github.com>
2019-11-26 00:03:16 +0400

    Add files via upload

---
+++ precache-manifest.d13dd41cc4ad0e40bb0410cc35200b68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a24d2b630d6d9a33af6f53123ae47b69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fdd0116fd0fbc8ab64b",
+    "url": "/static/css/main.1b3c19e3.chunk.css"
+  },
+  {
+    "revision": "359495000c29a88383d5",
+    "url": "/static/js/2.e7d09fd5.chunk.js"
+  },
+  {
+    "revision": "0fdd0116fd0fbc8ab64b",
+    "url": "/static/js/main.b0bf0c8c.chunk.js"
+  },
+  {
+    "revision": "86d553a244f3e055c7b5",
+    "url": "/static/js/runtime-main.84551a55.js"
+  },
+  {
+    "revision": "386c8ff06aaa9d3ea14528a5703b6ec5",
+    "url": "/static/media/Proxima_Nova.386c8ff0.ttf"
+  },
+  {
+    "revision": "3b8ad90a7a5366149b0595e667396d65",
+    "url": "/static/media/error.3b8ad90a.png"
+  },
+  {
+    "revision": "9e8a2b3937952b3de7aca04cb0d74020",
+    "url": "/static/media/logo.9e8a2b39.svg"
+  }
+]);
